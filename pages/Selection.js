230465a30900f9932e6140b39268a767c4ea440b
narudesigns <ibeabuchipaul@gmail.com>
2021-11-17 00:32:13 +0100

    concluded multiplayer online form project

---
+++ pages/Selection.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./Selection.module.css";
+
+export default function Selection({name, color}) {
+  return (
+    <div className={styles.selection}>
+      <div
+        className={styles.selection_border}
+        style={{
+          borderColor: color,
+        }}
+      />
+      <div className={styles.selection_name} style={{ background: color }}>
+        {name}
+      </div>
+    </div>
+  );
+}
---
+++ pages/api/auth.js
@@ -0,0 +1,32 @@
+import { authorize } from "@liveblocks/node";
+
+//const API_KEY = process.env.LIVEBLOCKS_SECRET_KEY;
+const API_KEY = "sk_test__s-YVXfyIVMnc1olozV2howt";
+
+export default async function auth(req, res) {
+  if (!API_KEY) {
+    return res.status(403).end();
+  }
+
+  // For the avatar example, we're generating random users
+  // and set their info from the authentication endpoint
+  // See https://liveblocks.io/docs/api-reference/liveblocks-node#authorize for more information
+
+  const response = await authorize({
+    room: req.body.room,
+    secret: API_KEY,
+    userInfo: {
+      name: NAMES[Math.floor(Math.random() * NAMES.length)],
+      picture: `/assets/avatars/${Math.floor(Math.random() * 10)}.png`,
+    }
+  });
+  return res.status(response.status).end(response.body);
+}
+
+const NAMES = [
+  "Ikeh Akinyemi",
+  "Naru Designs",
+  "Steve Fabre",
+  "Guillaume Salles",
+  "Fortune Kay"
+];
